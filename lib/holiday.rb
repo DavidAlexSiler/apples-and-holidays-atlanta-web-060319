1be8df6c9e87a1fa07f8a7a8d756190d5eada630
Alex Siler <davidalexsiler@gmail.com>
2019-06-02 03:41:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,8 +59,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, data|
     puts "#{season.to_s.capitalize!}:"
-       data.each do |holiday, supplies|
-         
+       data.each do |holiday, supply|
+         new_arr = holiday
         
     
 end
