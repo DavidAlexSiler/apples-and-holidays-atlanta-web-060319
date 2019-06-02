956a885d0be7f2a505abc62e246631b11ad09f24
Alex Siler <davidalexsiler@gmail.com>
2019-06-02 03:43:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
           new_arr.each do |x|
             formatted_holiday << x.capitalize!
           end
-          holiday = 
+          holiday = formatted_holiday.join(" ")
         
     
 end
