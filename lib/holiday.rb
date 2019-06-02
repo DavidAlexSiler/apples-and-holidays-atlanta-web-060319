c424f5b99b8352ef40c1185f23ea9e0408ae0e8a
Alex Siler <davidalexsiler@gmail.com>
2019-06-02 03:36:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |holiday, supplies|
-    puts #{holiday.to_s.capitalize!}
+    puts "#{holiday.to_s.capitalize!}"
     
 end
 
